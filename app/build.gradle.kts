2777da2b2003b3dbf94fbf2b1c779eef2b36cc0e
Cole Blvck <coleblvck@gmail.com>
2024-07-21 18:27:23 +0100

    1.0.0-alpha02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.coleblvck.shelfSlim"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0-alpha01"
+        versionCode = 2
+        versionName = "1.0.0-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
